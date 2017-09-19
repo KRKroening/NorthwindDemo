271fd262c52ea7883efcc777634af1d33b92511c
KRKroening <kimberlykroening@hotmail.com>
2017-09-19 11:45:23 -0600

    ddl

---
+++ Northwind.Data/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind.Data
+{
+    [Table("Category")]
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+
+        public virtual ICollection<Product> Product { get; set; }
+    }
+
+}
---
+++ Northwind.Data/NorthwindContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Data
+{
+    public class NorthwindContext : DbContext
+    {
+        public NorthwindContext() : base("name=NW")
+        {
+            Database.SetInitializer<NorthwindContext>(null);
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
